8e209bd0f2e571549e2372459fa2c82114441361
annachen88 <annachen880522@gmail.com>
2018-10-25 22:41:03 +0800

    IntentLogin

---
+++ app/src/main/java/ana/com/atm/LoginActivity.java
@@ -0,0 +1,24 @@
+package ana.com.atm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+    public void login(View view){
+        String user = ((EditText) findViewById(R.id.userId)).getText().toString();
+        //user.getText().toString();
+        String password = ((EditText)findViewById(R.id.passWord)).getText().toString();
+        if ("jack".equals(user) && "1234".equals(password)) {
+            finish();
+        }
+
+    }
+}
--- app/src/main/java/ana/com/atm/MainActivity.java
+++ app/src/main/java/ana/com/atm/MainActivity.java
@@ -1,13 +1,18 @@
 package ana.com.atm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean login = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(!login){
+            Intent intent = new Intent(this,LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
