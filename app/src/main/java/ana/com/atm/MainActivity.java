1df0f785adc0bf05b50d6f0c5439297136de9631
annachen88 <annachen880522@gmail.com>
2018-10-25 22:47:24 +0800

    btnMethod

--- app/src/main/java/ana/com/atm/MainActivity.java
+++ app/src/main/java/ana/com/atm/MainActivity.java
@@ -3,6 +3,7 @@ package ana.com.atm;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     boolean login = false;
@@ -14,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this,LoginActivity.class);
             startActivity(intent);
         }
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
     }
 }
