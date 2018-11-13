2a4e6ba675e9fac1459dd1e25c5c4deb79fc203f
annachen88 <annachen880522@gmail.com>
2018-11-13 16:05:23 +0800

    sharedPreference

--- app/src/main/java/ana/com/atm/LoginActivity.java
+++ app/src/main/java/ana/com/atm/LoginActivity.java
@@ -1,23 +1,35 @@
 package ana.com.atm;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText u;
+    EditText p;
+    String user;
+    String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        u = findViewById(R.id.userId);
+        p = findViewById(R.id.passWord);
+        String userId = getSharedPreferences("atm", MODE_PRIVATE).getString("USERID", "");
+       // userId.getString("USERID", "");
+        u.setText(userId);
     }
-    public void login(View view){
-        String user = ((EditText) findViewById(R.id.userId)).getText().toString();
-        //user.getText().toString();
-        String password = ((EditText)findViewById(R.id.passWord)).getText().toString();
+
+    public void login(View view) {
+        user = u.getText().toString();
+        password = p.getText().toString();
         if ("jack".equals(user) && "1234".equals(password)) {
             setResult(RESULT_OK);
+            getSharedPreferences("atm", MODE_PRIVATE).edit().putString("USERID", user).apply();
+
             finish();
         }
 
