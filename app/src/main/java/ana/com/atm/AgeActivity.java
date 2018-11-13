826bc768831e662c0d8a02221926a16ddf690e42
annachen88 <annachen880522@gmail.com>
2018-11-13 16:31:48 +0800

    SharedPreferences

--- app/src/main/java/ana/com/atm/AgeActivity.java
+++ app/src/main/java/ana/com/atm/AgeActivity.java
@@ -1,7 +1,9 @@
 package ana.com.atm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AgeActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class AgeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_age);
+    }
+
+    public void next2(View view) {
+        Intent gender = new Intent(AgeActivity.this,GenderActivity.class);
+        startActivity(gender);
     }
 }
