4c1f6199d1401d3ebaba61c9340a9e24fc5dcfc1
annachen88 <annachen880522@gmail.com>
2018-11-19 22:36:01 +0800

    setFlagsToMain

--- app/src/main/java/ana/com/atm/GenderActivity.java
+++ app/src/main/java/ana/com/atm/GenderActivity.java
@@ -1,7 +1,11 @@
 package ana.com.atm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP;
 
 public class GenderActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class GenderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gender);
+    }
+
+    public void next4(View view) {
+        Intent main = new Intent(GenderActivity.this,MainActivity.class);
+        main.setFlags(FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(main);
     }
 }
