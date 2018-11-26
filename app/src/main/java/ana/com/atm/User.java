0d48dbed3e16e032c1b43401292c2628d8e71380
annachen88 <annachen880522@gmail.com>
2018-11-26 23:17:34 +0800

    User

--- app/src/main/java/ana/com/atm/User.java
+++ app/src/main/java/ana/com/atm/User.java
@@ -1,10 +1,22 @@
 package ana.com.atm;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class User {
+    private Context context;
     String id;
-    String nickName;
+    String nickname;
     int age;
     int gender;
+    String address;
+    SharedPreferences settings;
+    boolean valid = false;
+
+    public User(Context context){
+        this.context = context;
+        settings = context.getSharedPreferences("user",Context.MODE_PRIVATE);
+    }
 
     public String getId() {
         return id;
@@ -15,26 +27,59 @@ public class User {
     }
 
     public String getNickName() {
-        return nickName;
+        if (nickname == null) {
+            nickname = settings.getString("NICKNAME", null);
+        }
+        return nickname;
+
     }
 
     public void setNickName(String nickName) {
-        this.nickName = nickName;
+        settings.edit()
+                .putString("NICKNAME", nickname).apply();
+        this.nickname = nickname;
     }
 
     public int getAge() {
+        if (age == 0) {
+            age = settings.getInt("AGE", 0);
+        }
         return age;
     }
 
     public void setAge(int age) {
+        settings.edit()
+                .putInt("AGE", age).apply();
         this.age = age;
     }
 
     public int getGender() {
+        if (gender == 0) {
+            gender = settings.getInt("GENDER", 0);
+        }
         return gender;
     }
 
     public void setGender(int gender) {
+        settings.edit()
+                .putInt("GENDER", gender).apply();
         this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public void setValid(boolean valid) {
+        this.valid = valid;
     }
 }
